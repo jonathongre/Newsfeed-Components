0b8aece046a711fc467a1048d5b66aef8e169511
Jonathon Green <ericg9811@gmail.com>
2019-07-11 10:14:33 -0500

    Really fixed list items not showing

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -35,36 +35,24 @@ let menuItems = [
 const menuDrop = document.querySelector('.header')
 
 menuItems.forEach(items => {
-            menuDrop.append(createMenu(items))
-        })
+    menuDrop.append(createMenu(items))
+})
 
 function createMenu(items) {
     const menu = document.createElement('div')
     const menuList = document.createElement('ul')
-    const menuItem1 = document.createElement('li')
-    const menuItem2 = document.createElement('li')
-    const menuItem3 = document.createElement('li')
-    const menuItem4 = document.createElement('li')
-    const menuItem5 = document.createElement('li')
-    const menuItem6 = document.createElement('li')
     const menuButton = document.querySelector('.menu-button')
 
     menu.appendChild(menuList)
-    menuList.appendChild(menuItem1)
-    menuList.appendChild(menuItem2)
-    menuList.appendChild(menuItem3)
-    menuList.appendChild(menuItem4)
-    menuList.appendChild(menuItem5)
-    menuList.appendChild(menuItem6)
 
     menu.classList.add('menu')
 
-    menuItem1.textContent = menuItems[0]
-    menuItem2.textContent = menuItems[1]
-    menuItem3.textContent = menuItems[2]
-    menuItem4.textContent = menuItems[3]
-    menuItem5.textContent = menuItems[4]
-    menuItem6.textContent = menuItems[5]
+    menuItems.forEach(menuItem => {
+        let listItem = document.createElement('li')
+        menuList.appendChild(listItem)
+        listItem.textContent = menuItem
+        listItem.classList.add('li')
+    })
 
     menuButton.addEventListener('click', event => {
         menu.classList.toggle('menu--open')
